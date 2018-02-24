1a2d2748e3a84e7c1e1adc0e25607dd9c81f4561
Noah <nhornak99@gmail.com>
2018-02-24 13:05:07 -0500

    Wrote campgroundCount test

--- Capstone.Tests/CampgroundTests.cs
+++ Capstone.Tests/CampgroundTests.cs
@@ -1,14 +1,58 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+using System.Transactions;
+using System.Configuration;
+using Capstone.DAL;
+using Capstone.Models;
+using System.Collections.Generic;
+using Capstone;
 
 namespace Capstone.Tests
 {
 	[TestClass]
 	public class CampgroundTests
 	{
-		[TestMethod]
-		public void TestMethod1()
+        string connectionString = ConfigurationManager.ConnectionStrings["CapstoneDatabase"].ConnectionString;
+
+        [TestMethod]
+		public void ReturnsCampgroundsCorrectly()
 		{
+            // Arrange
+            CampgroundDAL testClass = new CampgroundDAL();
+
+            // Act
+            int expectedParkCount1 = GetCampgroundCount(1);
+            List<Campground> campgrounds1 = testClass.GetAllCampgrounds(1);
+            int expectedParkCount2 = GetCampgroundCount(2);
+            List<Campground> campgrounds2 = testClass.GetAllCampgrounds(2);
+            
+
+            // Assert
+            Assert.AreEqual(expectedParkCount1, campgrounds1.Count);
+            Assert.AreEqual(expectedParkCount2, campgrounds2.Count);
 		}
+
+
+        public static int GetCampgroundCount(int parkId)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["CapstoneDatabase"].ConnectionString;
+
+            using (SqlConnection conn= new SqlConnection(connectionString))
+            {
+                int count = 0;
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) AS 'campgroundCount' FROM campground WHERE park_id=@parkId;", conn);
+                cmd.Parameters.AddWithValue("@parkId", parkId);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    count = Convert.ToInt32(reader["campgroundCount"]);
+                }
+
+                return count;
+            }
+        }
 	}
 }
